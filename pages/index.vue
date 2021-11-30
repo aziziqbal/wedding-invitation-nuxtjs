03b8998cfb7ec283eaf38cfe0472e8508b6c364a
aziziqball <muhaziziqbal06@gmail.com>
2021-11-30 22:20:58 +0700

    add particlejs

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
         class="gambar"
           :src="require('../static/new/bg_4.jpg')"
           height="100vh" style="">
+          <div id="particles-js"></div>
           <v-row data-aos="fade-up" align="center" justify="center" class="text-center fill-height pa-md-5 pa-3 mx-0">
             <v-col cols="12">
               <v-img height="200" :src="require('../static/new/user2.png')" contain />
@@ -31,6 +32,7 @@
 </template>
 
 <script>
+import 'particles.js'
 export default {
   data() {
     return {
@@ -40,18 +42,132 @@ export default {
   },
   mounted() {
     this.name = this.$router.currentRoute.query['to']
+
+    this.initParticles()
   },
   methods: {
     toHome () {
       this.$router.push({ name: 'main' })
       this.$root.$refs.A.openFullscreen()
+    },
+    initParticles() {
+      window.particlesJS("particles-js", {
+                "particles": {
+    "number": {
+      "value": 400,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 3,
+        "color": "#fff"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "http://www.dynamicdigital.us/wp-content/uploads/2013/02/starburst_white_300_drop_2.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.7,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 20,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 50,
+      "color": "#ffffff",
+      "opacity": 0.6,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 5,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 300,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode":  "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 150,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 200,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.2
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+            })
     }
   }
 }
 </script>
 
 <style scoped>
-#tsparticles{
+#particles-js{
   position: absolute;
   background-size: cover;
   top: 0;
